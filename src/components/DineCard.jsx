99601c1b125cc885486c371f2460a30ba271dcf2
divashusaini <dishusaini2july@gmail.com>
2026-06-23 05:15:10 +0530

    minor changes in DIneout

--- src/components/DineCard.jsx
+++ src/components/DineCard.jsx
@@ -5,9 +5,11 @@ export default function DineCard({ rest }) {
         
         <div className=" relative">
             <img className="w-80 h-50 object-cover" src={"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/"+rest?.info?.mediaFiles[0]?.url} alt="" />
-            <p className="absolute bottom-2 left-4 text-2xl text-white">{rest?.info?.name}</p>
-            <p className="absolute bottom-2 right-4 text-2xl text-white">{rest?.info?.rating?.value}</p>
+            <p className="absolute bottom-2 left-2 text-[18] text-white z-10">{rest?.info?.name}</p>
+            <p className="absolute bottom-2 right-2 text-xl text-white z-10">{rest?.info?.rating?.value}</p>
+              <div className="absolute bottom-0 left-0 right-0 h-16 bg-gradient-to-t from-black to-transparent  "></div>
         </div>
+      
     </div>
   );
 }
